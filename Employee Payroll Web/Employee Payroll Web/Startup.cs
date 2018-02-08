a34a0166265e3cf0a0eabb96b027819d72ce70cc
Mahfuzur-Rahman <7mahfuzur.rahman@gmail.com>
2018-02-09 01:45:34 +0600

    Employee Payroll Web Application Starts

---
+++ Employee Payroll Web/Employee Payroll Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Employee_Payroll_Web.Startup))]
+namespace Employee_Payroll_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
